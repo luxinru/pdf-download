67f4208d69bc0617859237c6f37ccca2eda0fbf1
yihong <macroyep@gmail.com>
2019-04-18 18:53:09 +0800

    支持环境变量文件

--- env.js
+++ env.js
@@ -1,6 +1,28 @@
-export const TOKEN =
-  'Rjg5MDRGRjdGOUJDNjFDQkM0NzI3MTZDQ0E0RjQxRkNBODMyNkFBRjkyOEQ1Qjg3QkIxMUFEQjAxQjcyRDRCQw==';
+import fs from 'fs';
+const ENV_MAP = parseEnv();
 
-export const PAGE_URL_ROOT = 'http://localhost:8080';
+export const TOKEN = env('TOKEN', 'token');
 
-export const STORAGE_ROOT = 'storage';
+export const PAGE_URL_ROOT = env('PAGE_URL_ROOT', 'http://localhost:8080');
+
+export const STORAGE_ROOT = env('storage', 'storage');
+
+function env(key, defaultValue) {
+  return ENV_MAP[key] || defaultValue;
+}
+
+export default env;
+
+function parseEnv() {
+  const text = fs.readFileSync('./.env').toString();
+  const row = text.split('\n');
+  const map = {};
+  row.forEach(item => {
+    // 去掉注释
+    if (item[0] !== '#') {
+      const [key, value] = item.split('=');
+      map[key] = value;
+    }
+  });
+  return map;
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# 数据可视化平台 Node 服务
+
+#### Usage
+
+`npm i`
+
+`npm run start`
+
+#### API
+
+1.call
+
+2.callback
