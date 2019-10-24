1d5c0f8d1501adeb938774c5d8ecc479cba6c998
Luxinru <luxinru@xlink.cn>
2019-10-24 17:54:56 +0800

    更新导出订阅逻辑

--- jobs/RenderComponentJob.js
+++ jobs/RenderComponentJob.js
@@ -1,102 +1,73 @@
-import Job from './Job';
-import { CACHE_PATH, PAGE_URL_ROOT, CHROMIUM_URI } from '../env';
-import puppeteer from 'puppeteer-core';
-import UploadFileJob from './UploadFileJob';
-import path from 'path';
-import log from '../logs';
+import Job from './Job'
+import { CACHE_PATH, PAGE_URL_ROOT, CHROMIUM_URI } from '../env'
+import UploadFileJob from './UploadFileJob'
+import path from 'path'
+import log from '../logs'
 
 /**
  * 渲染仪表盘
  */
 export default class extends Job {
   constructor(accessToken, id, taskId, taskType) {
-    super();
-    this.accessToken = accessToken;
-    this.id = id;
-    this.taskId = taskId;
+    super()
+    this.accessToken = accessToken
+    this.id = id
+    this.taskId = taskId
     this.taskType = taskType
   }
 
   async run() {
-    const url = `${PAGE_URL_ROOT}/show.html#/component/token/${
-      this.accessToken
-    }/id/${this.id}`;
+    const url = `${PAGE_URL_ROOT}/show.html#/component/token/${this.accessToken}/id/${this.id}`
 
-    log.info('导出页面:' + url);
+    log.info('导出页面:' + url)
+    console.log('导出页面:' + url)
 
-    const options = {
-      defaultViewport: {
-        width: 1920,
-        height: 1080
-      },
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-      ]
-    };
+    const browser = global.browser
 
     // 使用离线下载的Chromium
-    options.executablePath = path.resolve(__dirname, '../' + CHROMIUM_URI);
-    let browser = await puppeteer.launch(options);
-    let page = await browser.newPage();
+    let page = await browser.newPage()
     await page.goto(url, {
       waitUntil: ['networkidle0']
-    });
+    })
 
-    // 获取页面高度，用于动态设置pdf高度
-    const { bodyHeight, title } = await page.evaluate(() => {
+    // 获取页面宽度 高度，用于动态设置pdf高度
+    const { bodyWeight, bodyHeight, title } = await page.evaluate(() => {
+      const el = document.getElementById('tableId')
       return {
         bodyHeight: document.body.clientHeight,
-        title: window._title
-      };
-    });
-
-    // 获取页面高度，用于动态设置pdf高度
-    const contentWidth = await page.evaluate(() => {
-      const el = document.getElementById('tableId')
-      if (el) {
-        return el.firstChild.scrollWidth
-      } else {
-        return 1920
+        title: window._title,
+        bodyWeight: el.firstChild.scrollWidth
       }
-    });
+    })
 
-    if (contentWidth && contentWidth > 1920) {
-      await browser.close()
-      browser = await puppeteer.launch({
-        defaultViewport: {
-          width: contentWidth + 20,
-          height: bodyHeight
-        },
-        executablePath: path.resolve(__dirname, '../' + CHROMIUM_URI)
+    if (bodyWeight && bodyWeight > 1920) {
+      await page.setViewport({
+        width: bodyWeight + 20,
+        height: bodyHeight
       })
-      page = await browser.newPage();
-      await page.goto(url, {
-        waitUntil: ['networkidle0']
-      });
     }
-    
+
     // 构建文件名称
-    const filePath = `${CACHE_PATH}/${title}-${this.taskId}.pdf`;
-    
+    const filePath = `${CACHE_PATH}/${title}-${this.taskId}.pdf`
+
     await page.pdf({
       path: filePath,
       printBackground: true,
       // scale: 1,
-      width: contentWidth && contentWidth > 1920 ? contentWidth + 'px' : 1920 + 'px',
+      width: bodyWeight && bodyWeight > 1920 ? bodyWeight + 'px' : 1920 + 'px',
       height: bodyHeight + 10 + 'px'
-    });
+    })
 
-    await browser.close();
-    log.info('PDF地址:' + filePath);
+    await page.close()
+    log.info('PDF地址:' + filePath)
 
     Job.dispatch(
       new UploadFileJob(
         this.accessToken,
         path.resolve(__dirname, '../' + filePath),
         this.taskId,
-        this.taskType,
+        this.taskType
       )
-    );
+    )
   }
 }
--- jobs/RenderDashboardJob.js
+++ jobs/RenderDashboardJob.js
@@ -1,61 +1,40 @@
-import Job from './Job';
-import { CACHE_PATH, PAGE_URL_ROOT, CHROMIUM_URI } from '../env';
-import puppeteer from 'puppeteer-core';
-import UploadFileJob from './UploadFileJob';
-import path from 'path';
-import log from '../logs';
+import Job from './Job'
+import { CACHE_PATH, PAGE_URL_ROOT, CHROMIUM_URI } from '../env'
+import UploadFileJob from './UploadFileJob'
+import path from 'path'
+import log from '../logs'
 
 /**
  * 渲染仪表盘
  */
 export default class extends Job {
   constructor(accessToken, id, taskId) {
-    super();
-    this.accessToken = accessToken;
-    this.id = id;
-    this.taskId = taskId;
+    super()
+    this.accessToken = accessToken
+    this.id = id
+    this.taskId = taskId
   }
 
   async run() {
-    const url = `${PAGE_URL_ROOT}/show.html#/dashboard/token/${
-      this.accessToken
-    }/id/${this.id}`;
+    const url = `${PAGE_URL_ROOT}/show.html#/dashboard/token/${this.accessToken}/id/${this.id}`
 
-    log.info('导出页面:' + url);
+    log.info('导出页面:' + url)
 
-    const options = {
-      defaultViewport: {
-        width: 1920,
-        height: 1080
-      },
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-      ]
-    };
+    const browser = global.browser
+    const page = await browser.newPage()
 
-    // 使用离线下载的Chromium
-    if (CHROMIUM_URI[0] === '/') {
-      // 使用绝对路径
-      options.executablePath = path.resolve(CHROMIUM_URI);
-    } else {
-      // 使用相对路径
-      options.executablePath = path.resolve(__dirname, '../' + CHROMIUM_URI);
-    }
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
     await page.goto(url, {
       waitUntil: ['networkidle0']
-    });
+    })
 
     // 获取页面高度，用于动态设置pdf高度
     const { bodyHeight } = await page.evaluate(() => {
       return {
         bodyHeight: document.body.clientHeight
-      };
-    });
+      }
+    })
     // 构建文件名称
-    const filePath = `${CACHE_PATH}/${Date.now()}-${this.taskId}.pdf`;
+    const filePath = `${CACHE_PATH}/${Date.now()}-${this.taskId}.pdf`
 
     await page.pdf({
       path: filePath,
@@ -63,15 +42,16 @@ export default class extends Job {
       // scale: 1,
       width: '1920px',
       height: bodyHeight + 10 + 'px'
-    });
+    })
 
-    await browser.close();
-    log.info('PDF地址:' + filePath);
+    await page.close()
+
+    log.info('PDF地址:' + filePath)
     const fileName =
       CACHE_PATH[0] === '/'
         ? path.resolve(filePath)
-        : path.resolve(__dirname, '../' + filePath);
+        : path.resolve(__dirname, '../' + filePath)
 
-    Job.dispatch(new UploadFileJob(this.accessToken, fileName, this.taskId));
+    Job.dispatch(new UploadFileJob(this.accessToken, fileName, this.taskId))
   }
 }
