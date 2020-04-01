6ddaffa66e298529457e7169d09b0bf7442e08d5
陆信儒 <luxinru@xlink.cn>
2020-04-01 10:46:20 +0800

    feat: 项目数据报表支持项目数据筛选

--- controllers/ExportController.js
+++ controllers/ExportController.js
@@ -1,8 +1,8 @@
-import Controller from './Controller';
-import Job from '../jobs/Job';
-import RenderDashboardJob from '../jobs/RenderDashboardJob';
-import SchedulerCallbackJob from '../jobs/SchedulerCallbackJob';
-import RenderComponentJob from '../jobs/RenderComponentJob';
+import Controller from "./Controller";
+import Job from "../jobs/Job";
+import RenderDashboardJob from "../jobs/RenderDashboardJob";
+import SchedulerCallbackJob from "../jobs/SchedulerCallbackJob";
+import RenderComponentJob from "../jobs/RenderComponentJob";
 
 /**
  * 导出仪表盘
@@ -14,32 +14,42 @@ export default class ExportController extends Controller {
    * @param {*} dashboardId
    */
   async index() {
-    const accessToken = this.req.get('Access-Token');
+    const accessToken = this.req.get("Access-Token");
     const valid = this.validator({
-      task_id: { type: 'string' },
-      object_id: { type: 'string' },
-      object_type: { type: 'enum', values: ['dashboard', 'component'] }
+      task_id: { type: "string" },
+      object_id: { type: "string" },
+      object_type: { type: "enum", values: ["dashboard", "component"] },
+      project_id: { type: "string" }
     });
     if (!valid) return;
 
-    const taskId = this.request('task_id');
-    const objectId = this.request('object_id');
-    const objectType = this.request('object_type');
-    const taskType = this.request('task_type');
-    if (objectType === 'dashboard') {
+    const taskId = this.request("task_id");
+    const objectId = this.request("object_id");
+    const objectType = this.request("object_type");
+    const projectId = this.request("project_id");
+    const taskType = this.request("task_type");
+    if (objectType === "dashboard") {
       Job.dispatch(new RenderDashboardJob(accessToken, objectId, taskId));
-      this.resCodeJson({ msg: '导出任务提交成功', code: 1 });
-    } else if (objectType === 'component') {
-      Job.dispatch(new RenderComponentJob(accessToken, objectId, taskId, taskType));
-      this.resCodeJson({ msg: '导出任务提交成功', code: 1 });
+      this.resCodeJson({ msg: "导出任务提交成功", code: 1 });
+    } else if (objectType === "component") {
+      Job.dispatch(
+        new RenderComponentJob(
+          accessToken,
+          objectId,
+          taskId,
+          projectId,
+          taskType
+        )
+      );
+      this.resCodeJson({ msg: "导出任务提交成功", code: 1 });
     } else {
       this.resErrJson(400001, `object_type: ${objectType} 尚未支持`);
     }
   }
 
   test() {
-    const accessToken = this.req.get('Access-Token');
+    const accessToken = this.req.get("Access-Token");
     Job.dispatch(new SchedulerCallbackJob(accessToken, 234234423, 23434223));
-    this.resCodeJson({ msg: '导出任务提交成功', code: 1 });
+    this.resCodeJson({ msg: "导出任务提交成功", code: 1 });
   }
 }
--- jobs/RenderComponentJob.js
+++ jobs/RenderComponentJob.js
@@ -1,27 +1,31 @@
-import Job from './Job'
-import { CACHE_PATH, PAGE_URL_ROOT, CHROMIUM_URI } from '../env'
-import UploadFileJob from './UploadFileJob'
-import path from 'path'
-import log from '../logs'
-var puppeteer = require('puppeteer-core')
+import Job from "./Job";
+import { CACHE_PATH, PAGE_URL_ROOT, CHROMIUM_URI } from "../env";
+import UploadFileJob from "./UploadFileJob";
+import path from "path";
+import log from "../logs";
+var puppeteer = require("puppeteer-core");
 
 /**
  * 渲染仪表盘
  */
 export default class extends Job {
-  constructor(accessToken, id, taskId, taskType) {
-    super()
-    this.accessToken = accessToken
-    this.id = id
-    this.taskId = taskId
-    this.taskType = taskType
+  constructor(accessToken, id, taskId, projectId, taskType) {
+    super();
+    this.accessToken = accessToken;
+    this.id = id;
+    this.taskId = taskId;
+    this.projectId = projectId;
+    this.taskType = taskType;
   }
 
   async run() {
-    const url = `${PAGE_URL_ROOT}/show.html#/component/token/${this.accessToken}/id/${this.id}`
+    const url =
+      this.projectId && this.projectId !== "null"
+        ? `${PAGE_URL_ROOT}/show.html#/component/token/${this.accessToken}/id/${this.id}?projectId=${this.projectId}`
+        : `${PAGE_URL_ROOT}/show.html#/component/token/${this.accessToken}/id/${this.id}`;
 
-    log.info('导出页面:' + url)
-    console.log('导出页面:' + url)
+    log.info("导出页面:" + url);
+    console.log("导出页面:" + url);
 
     if (!global.browser) {
       puppeteer
@@ -30,60 +34,60 @@ export default class extends Job {
             width: 1920,
             height: 1080
           },
-          args: ['--no-sandbox', '--disable-setuid-sandbox'],
-          executablePath: path.resolve(__dirname, './' + CHROMIUM_URI)
+          args: ["--no-sandbox", "--disable-setuid-sandbox"],
+          executablePath: path.resolve(__dirname, "./" + CHROMIUM_URI)
         })
         .then(res => {
-          global.browser = res
-        })
+          global.browser = res;
+        });
     }
 
-    const browser = global.browser
+    const browser = global.browser;
 
     // 使用离线下载的Chromium
-    let page = await browser.newPage()
+    let page = await browser.newPage();
     await page.goto(url, {
-      waitUntil: ['networkidle0']
-    })
+      waitUntil: ["networkidle0"]
+    });
 
     // 获取页面宽度 高度，用于动态设置pdf高度
     const { bodyWeight, bodyHeight, title } = await page.evaluate(() => {
-      const el = document.getElementById('tableId')
+      const el = document.getElementById("tableId");
       return {
         bodyHeight: document.body.clientHeight,
         title: window._title,
         bodyWeight: el.firstChild.scrollWidth
-      }
-    })
+      };
+    });
 
     if (bodyWeight && bodyWeight > 1920) {
       await page.setViewport({
         width: bodyWeight + 20,
         height: bodyHeight
-      })
+      });
     }
 
     // 构建文件名称
-    const filePath = `${CACHE_PATH}/${title}-${this.taskId}.pdf`
+    const filePath = `${CACHE_PATH}/${title}-${this.taskId}.pdf`;
 
     await page.pdf({
       path: filePath,
       printBackground: true,
       // scale: 1,
-      width: bodyWeight && bodyWeight > 1920 ? bodyWeight + 'px' : 1920 + 'px',
-      height: bodyHeight + 10 + 'px'
-    })
+      width: bodyWeight && bodyWeight > 1920 ? bodyWeight + "px" : 1920 + "px",
+      height: bodyHeight + 10 + "px"
+    });
 
-    await page.close()
-    log.info('PDF地址:' + filePath)
+    await page.close();
+    log.info("PDF地址:" + filePath);
 
     Job.dispatch(
       new UploadFileJob(
         this.accessToken,
-        path.resolve(__dirname, '../' + filePath),
+        path.resolve(__dirname, "../" + filePath),
         this.taskId,
         this.taskType
       )
-    )
+    );
   }
 }
